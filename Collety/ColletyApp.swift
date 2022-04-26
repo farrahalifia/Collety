28d9b0bbbe5cfafaf994a768320764be046aad2b
Farrah Alifia <fputri22@bsd.idserve.net>
2022-04-26 21:03:06 +0700

    Initial Commit

---
+++ Collety/ColletyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColletyApp.swift
+//  Collety
+//
+//  Created by Farrah Alifia on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ColletyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Collety/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Collety
+//
+//  Created by Farrah Alifia on 26/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
